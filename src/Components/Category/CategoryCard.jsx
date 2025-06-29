e71345324e82cd4f2bccbea5e437cb4f99b0a965
Beth-22 <nate85here@gmail.com>
2025-06-29 18:12:02 +0300

    bugs

--- src/Components/Category/CategoryCard.jsx
+++ src/Components/Category/CategoryCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./CategoryCard.module.css";
+import styles from "./categoryCard.module.css";
 import {Link} from 'react-router-dom'
 export default function CategoryCard({ data }) {
   return (
